1a5d7129b944a141afba14d2b3ec61df09655a78
BugHunter-X <askzenai@gmail.com>
2025-01-28 02:22:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when using AsyncStorage in Expo and is related to the way data is handled in the background.  It's particularly noticeable when dealing with large amounts of data or frequent updates. The specific error message might vary, but it often involves issues like exceeding maximum storage capacity or unexpected data corruption.
